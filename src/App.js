36965fda29d3d270e79e3f15d55cc02b6d0846d0
Uhiene Gift Eleojo <eleojogiftuhiene@gmail.com>
2021-05-31 00:34:09 +0100

    added api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import awsConfig from "./aws-exports";
+import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { listLists } from "./graphql/queries";
 
-import './App.css';
+Amplify.configure(awsConfig);
 
 function App() {
+  const [list, setList] = useState([]);
+  async function fetchList() {
+    const {data} = await API.graphql(graphqlOperation(listLists));
+    setList(data.listLists.items)
+  }
+  useEffect(() => {
+    fetchList();
+  }, []);
   return (
-    <div className="App">
-     <h1>amp</h1>
-    </div>
+    <AmplifyAuthenticator>
+      <div className="App">
+        <h1>amp</h1>
+        <ul>
+          {list.map(item => <li key={item.id}>{item.title}</li>)}
+        </ul>
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
   );
 }
 
